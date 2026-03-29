61f0ef6e1ca424144ecec4b1219eca4ecece3591
ravikant1983 <111572726+ravikant1983@users.noreply.github.com>
2026-03-29 15:19:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /app/llama.cpp/build/bin/llama-server \
-  -m /app/llama.cpp/models/Llama-3.2-3B-Instruct-Q4_K_M.gguf \
+  -m /app/llama.cpp/models/ChatWithRavikant.gguf \
   -c 4096 \
   --host 0.0.0.0 \
   --port 8080
